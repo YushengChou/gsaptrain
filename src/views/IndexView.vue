67d108e97bf075e95aa69afcf786c4869e3df410
DESKTOP-RTCT2GL\user <mm7217373@gmail.com>
2022-11-28 18:37:23 +0800

    加入gsap

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,23 +1,126 @@
 <template>
   <header class="header">
     <div class="header_container">
-      <a href="logo">
-        <h1>LOGO</h1>
+      <a href="#">
+        <h1 class="logo">LOGO</h1>
       </a>
       <nav class="nav">
         <ul class="nav_list">
-          <li class="nav_item">work</li>
-          <li class="nav_item">services</li>
-          <li class="nav_item">pricing</li>
-          <li class="nav_item">network</li>
-          <li class="nav_item">start a project</li>
+          <li class="nav_item">
+            <a href="#">
+              work
+            </a>
+          </li>
+          <li class="nav_item">
+            <a href="#">
+              services
+            </a>
+          </li>
+          <li class="nav_item">
+            <a href="#">
+              pricing
+            </a>
+          </li>
+          <li class="nav_item">
+            <a href="#">
+              network
+            </a>
+          </li>
+          <li class="nav_item">
+            <a href="#">
+              start a project
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
   </header>
+  <div class="container">
+    <div class="box1" @mousemove="circleMove" @mouseleave="circleLeave">
+      <div class="circles">
+        <div class="circle circle1"></div>
+        <div class="circle circle2"></div>
+        <div class="circle circle3"></div>
+        <div class="circle circle4"></div>
+        <div class="circle circle5"></div>
+      </div>
+      <div class="box_item"></div>
+      <div class="box_item"></div>
+      <div class="box_item"></div>
+      <div class="box_item"></div>
+    </div>
+    <div class="blank">
+      <div class="blank_head">
+        Your&ensp;<span id="blank_text1">aaaaa</span>&ensp;made&ensp;<span>bbbbb</span>.
+      </div>
+      <div class="blank_body"></div>
+    </div>
+  </div>
 </template>
 
-<script></script>
+<script>
+import gsap from 'gsap'
+export default {
+  name: 'app',
+  mounted() {
+    gsap.to('h1', 1, {
+      color: 'red',
+      repeat: -1,
+    })
+    gsap.to('.blank_body', .8, {
+      y: 50,
+      opacity: 0,
+      repeat: -1,
+    })
+    gsap.to('#blank_text1', 1, {
+      opacity: 0.5,
+      repeat: -1,
+      yoyo: true,
+      onComplete: function() {
+        // document.getElementById('blank_text1').innerHTML = '!@#$@!$#%$'
+      }
+    })
+  },
+  methods: {
+    circleMove(e) {
+      gsap.to('.circle2', 1, {
+        x: e.pageX / 2,
+        y: e.pageY / 2
+      })
+      gsap.to('.circle3', 1, {
+        x: -e.pageX / 2,
+        y: e.pageY / 2
+      })
+      gsap.to('.circle4', 1, {
+        x: e.pageX / 2,
+        y: -e.pageY / 2
+      })
+      gsap.to('.circle5', 1, {
+        x: -e.pageX / 2,
+        y: -e.pageY / 2
+      })
+    },
+    circleLeave() {
+      gsap.to('.circle2', 1, {
+        x: 0,
+        y: 0
+      })
+      gsap.to('.circle3', 1, {
+        x: 0,
+        y: 0
+      })
+      gsap.to('.circle4', 1, {
+        x: 0,
+        y: 0
+      })
+      gsap.to('.circle5', 1, {
+        x: 0,
+        y: 0
+      })
+    },
+  },
+}
+</script>
 
 <style lang="sass" scoped>
 *
@@ -34,4 +137,81 @@ html
       color: #000
       display: block
       text-decoration: none
+    header
+      height: 6vh
+      .header_container
+        display: flex
+        justify-content: space-between
+        align-items: center
+        max-width: 100%
+        .logo
+          padding: 10px
+          font-size: 2rem
+        nav
+          .nav_list
+            display: flex
+            .nav_item
+              font-size: 1.2rem
+              text-align: center
+              margin: auto 
+              padding: 20px 10px
+              &:last-child
+                width: 150px
+                border-left: 1px solid #000
+    .container
+       max-width: 100%
+       .box1
+        position: relative
+        display: flex
+        flex-wrap: wrap
+        height: 84vh
+        overflow: hidden
+        .box_item
+          width: 50%
+          border: 1px solid #000
+        .circles
+          position: absolute
+          left: 50%
+          top: 50%
+          transform: translate(-50%, -50%)
+          height: 100%
+          .circle
+            position: absolute
+            left: 50%
+            top: 50%
+            transform: translate(-50%, -50%)
+            width: 42vh
+            height: 42vh
+            border: 1px solid #000
+            border-radius: 50%
+    .blank
+      display: flex
+      justify-content: space-between
+      align-items: center
+      overflow: hidden
+      padding: 10px
+      height: 10vh
+      .blank_head
+        font-size: 3rem
+      .blank_body
+        position: relative
+        transform: translate(-20px, -20px)
+        width: 5px
+        height: 40px
+        background-color: #000
+        &::before, &::after
+            position: absolute
+            top: 24px
+            display: block
+            content: ''
+            width: 5px
+            height: 20px
+            background-color: #000
+        &::before
+            left: 6px
+            transform: rotate(45deg)
+        &::after
+            left: -6px
+            transform: rotate(-45deg)
+
 </style>
