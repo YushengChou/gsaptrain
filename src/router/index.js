d645ee4f4ab8141da6767528e026fb1925e7da70
DESKTOP-RTCT2GL\user <mm7217373@gmail.com>
2022-11-28 10:53:01 +0800

    加入css reset, 建立indexView

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/index',
+    name: 'index',
+    component: () => import('../views/IndexView.vue')
+  },
 ]
 
 const router = createRouter({
---
+++ src/views/IndexView.vue
@@ -0,0 +1,37 @@
+<template>
+  <header class="header">
+    <div class="header_container">
+      <a href="logo">
+        <h1>LOGO</h1>
+      </a>
+      <nav class="nav">
+        <ul class="nav_list">
+          <li class="nav_item">work</li>
+          <li class="nav_item">services</li>
+          <li class="nav_item">pricing</li>
+          <li class="nav_item">network</li>
+          <li class="nav_item">start a project</li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script></script>
+
+<style lang="sass" scoped>
+*
+  box-sizing: border-box
+html
+  scroll-behavior: smooth
+  body
+    font-family: Verdana
+    line-height: 1.5
+    img
+      display: block
+      vertical-align: bottom
+    a
+      color: #000
+      display: block
+      text-decoration: none
+</style>
